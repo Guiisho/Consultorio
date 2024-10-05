98a1873f91cb2e065790e0a9aa2fd088bcd2d839
Guiisho <alarconguille556@gmail.com>
2024-10-05 02:12:57 -0300

    10k changes, login y register

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,17 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [provideRouter(routes), provideClientHydration(),
+     provideFirebaseApp(() => initializeApp({
+      "projectId":"consultorio-firebase-aut-4c49a",
+      "appId":"1:341509019103:web:178e9d8e196343a6b0206a",
+      "storageBucket":"consultorio-firebase-aut-4c49a.appspot.com",
+      "apiKey":"AIzaSyAINrap-l8ajdQNdlDk2nKYQjSE1kWFugA",
+      "authDomain":"consultorio-firebase-aut-4c49a.firebaseapp.com",
+      "messagingSenderId":"341509019103"})),
+       provideAuth(() => getAuth())]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,14 +4,14 @@ import { AboutComponent } from './Components/about/about.component';
 import { ContactComponent } from './Components/contact/contact.component';
 import { ProductosComponent } from './Components/productos/productos.component';
 import { ServiciosComponent } from './Components/servicios/servicios.component';
-import { CRUDComponent } from './Services/crud/crud.component';
-import { LogInComponent } from './Services/log-in/log-in.component';
-import { LogOutComponent } from './Services/log-out/log-out.component';
-import { RegisterComponent } from './Services/register/register.component';
+import { CRUDComponent } from './Components/crud/crud.component';
+import { LogInComponent } from './Components/log-in/log-in.component';
+import { LogOutComponent } from './Components/log-out/log-out.component';
+import { RegisterComponent } from './Components/register/register.component';
 
 export const routes: Routes = [
     
-    {path: '', component: HomeComponent},
+    {path: 'home', pathMatch: 'full', component: HomeComponent},
     {path: 'about', component: AboutComponent},
     {path: 'contact', component: ContactComponent},
     {path: 'productos', component: ProductosComponent},
@@ -20,5 +20,5 @@ export const routes: Routes = [
     {path: 'login', component: LogInComponent},
     {path: 'logout', component: LogOutComponent},
     {path: 'register', component: RegisterComponent},
-    {path: '**', redirectTo: 'Home'}
+    {path: '**', redirectTo: 'home'}
 ];
